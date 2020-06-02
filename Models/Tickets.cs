0845f8bb7b1ca6ee6b92099fb5c98dac54dc64cd
jggalmeida <j.g.almeida@hotmail.com>
2020-06-02 00:57:17 -0300

    Model Ticket Created

---
+++ Models/Tickets.cs
@@ -0,0 +1,16 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Ticket_Management_System.Models
+{
+    public class Ticket
+    {
+        public int Id { get; set; }
+        public string Title { get; set }
+        public string Description { get; set; }
+        public DateTime EnteredOn { get; set; }
+        public string Email { get; set; }
+        public string Priority { get; set; }
+    }
+
+}
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Ticket-Management-System
 TMS - Ticket Managemente System - Sistema para avaliação BuildOne
+
+# Docker - MSSQL Image
+# Azure Data Studio to create the Database
+# dotnet add package Microsoft.EntityFrameworkCore.SqlServer - EF package
