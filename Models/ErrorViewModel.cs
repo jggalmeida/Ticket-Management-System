8f2023980434385973c4b84740b156d86e1d1915
jggalmeida <j.g.almeida@hotmail.com>
2020-06-02 00:22:29 -0300

    Created MVC Application Standard

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Ticket_Management_System.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
