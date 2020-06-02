0246d765f6eaa949c240d379f3ce8110ff3547e6
jggalmeida <j.g.almeida@hotmail.com>
2020-06-02 00:58:29 -0300

    Model Ticket revised

--- Models/Tickets.cs
+++ Models/Tickets.cs
@@ -6,7 +6,7 @@ namespace Ticket_Management_System.Models
     public class Ticket
     {
         public int Id { get; set; }
-        public string Title { get; set }
+        public string Title { get; set; }
         public string Description { get; set; }
         public DateTime EnteredOn { get; set; }
         public string Email { get; set; }
